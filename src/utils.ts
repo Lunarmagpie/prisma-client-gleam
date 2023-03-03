78d998e8f9216c28e256dcab640011af3b4a91c9
Lunarmagpie <bambolambo0@gmail.com>
2023-03-03 11:43:07 -0500

    refactor readline

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
 export function read_stdin<T>(callback: (_: string) => T): T {
     const buf = new Uint8Array(1024);
     return Deno.stdin.read(buf).then(
-        n => {
-            if (n == Deno.EOF) {
-                return callback("ERROR")
+        next_line => {
+            if (next_line == Deno.EOF) {
+                return callback("")
             } else {
-                return callback(new TextDecoder().decode(buf.subarray(0, n)));
+                return callback(new TextDecoder().decode(buf.subarray(0, next_line)));
             }
         }
     );
